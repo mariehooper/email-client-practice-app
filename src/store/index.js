28b9b7fdee7a14296a46e13c72491f529b26cea7
Cameron Bothner <cameronbothner@gmail.com>
2018-01-14 15:42:54 -0500

    Add an example helper method of the type we might want

--- src/store/index.js
+++ src/store/index.js
@@ -9,4 +9,17 @@ import type { Data, Message } from './types';
 
 const store: Data = require('./emails.json');
 
+/**
+ * You might find it useful to implement some functions that make accessing
+ * deeply nested attributes of Messages or Threads...
+ */
+
+// For example, this returns the subject of a given message if it is defined
+// by a header in the message payload
+export function getMessageSubject(message: Message): ?string {
+  const { headers } = message.payload;
+  const subjectHeader = headers.find(header => header.name === 'Subject');
+  return subjectHeader && subjectHeader.value;
+}
+
 export default store;
