3eb94b9a8622d9e81e70961484f80bb7dfb8bbc9
Marie Hooper <marie.hooper@ithaka.org>
2018-01-15 14:51:31 -0500

    Update li to button

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ function renderSidebar() {
   const sidebarContents = `
     <h2 class="email-header">Inbox</h2>
     <ul class="email-list">
-      <li class="email-item">
-        <div class="sender-details">
-          <p>Email sender</p>
-          <span>Timestamp</span>
-        </div>
-        <p class="email-subject">Email1 subject</p>
-        <p>Email snippet</p>
+      <li>
+        <button class="email-item" type="button">
+          <div class="sender-details">
+            <p>Email sender</p>
+            <span>Timestamp</span>
+          </div>
+          <p class="email-subject">Email1 subject</p>
+          <p>Email snippet</p>
+        </button>
       </li>
     </ul>
   `;
