58002cf4fc4072ed71c0c3c6b88f57167666aa28
Marie Hooper <marie.hooper@ithaka.org>
2018-01-15 14:27:29 -0500

    Revise approach to render method to give less up front

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
-// helper funcs for creating elements
-function createElement(tag, className) {
-  const element = document.createElement(tag);
-  if (className) {
-    element.classList.add(className);
-  }
-  return element;
-}
-
-function createElementWithText(tag, text, className) {
-  const element = createElement(tag, className);
-  const elementText = document.createTextNode(text);
-  element.appendChild(elementText);
-  return element;
-}
-
 // example of generating HTML with js
-function generateEmailList() {
-  const emailList = createElement('ul', 'email-list');
-
-  [...Array(6).keys()].forEach((num) => {
-    const listItem = createElement('li');
-    const email = createElement('button', 'email-item');
-
-    const senderDetails = createElement('div', 'sender-details');
-    const sender = createElementWithText('p', 'Email sender');
-    const timestamp = createElementWithText('span', 'timestamp');
-    senderDetails.appendChild(sender);
-    senderDetails.appendChild(timestamp);
-    email.appendChild(senderDetails);
-
-    const emailSubject = createElementWithText(
-      'p',
-      `Email${num} subject`,
-      'email-subject',
-    );
-    email.appendChild(emailSubject);
-
-    const emailSnippet = createElementWithText('p', 'Email snippet');
-    email.appendChild(emailSnippet);
-    emailList.appendChild(listItem);
-    listItem.appendChild(email);
-  });
-  document.querySelector('.email-list-container').appendChild(emailList);
+function renderSidebar() {
+  const sidebarContents = `
+    <h2 class="email-header">Inbox</h2>
+    <ul class="email-list">
+      <li class="email-item">
+        <div class="sender-details">
+          <p>Email sender</p>
+          <span>Timestamp</span>
+        </div>
+        <p class="email-subject">Email1 subject</p>
+        <p>Email snippet</p>
+      </li>
+    </ul>
+  `;
+  document.querySelector('.email-list-container').innerHTML = sidebarContents;
 }
 
-generateEmailList();
+renderSidebar();
